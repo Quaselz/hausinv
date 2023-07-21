4413b60b7d55b3c18eb598d3cffa5c26355e55ab
Quaselz <paul.geppert94@freenet.de>
2023-07-21 13:46:32 +0200

    server api routen erstellt

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,31 @@
 import express from "express";
+import cors from "cors";
+import "./models/index.js";
+import morgan from "morgan";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+app.get("/api/inventar", (req, res) => {
+	res.send("get");
+});
+app.get("/api/inventar/:id", (req, res) => {
+	res.send("getSingle");
+});
+app.post("/api/inventar", (req, res) => {
+	res.send("post");
+});
+app.put("/api/inventar/:id", (req, res) => {
+	res.send("put");
+});
+app.delete("/api/inventar/:id", (req, res) => {
+	res.send("delete");
+});
+
+app.listen(port, () => {
+	console.log("tschuuu tschuuu");
+});
